8fc7ff11e573b79baffe5a5cd92875b2ddb66ac3
Sivakumar Sethuraj <ssethuraj@ghx.com>
2019-05-29 20:34:19 +0530

    implemented Fractions validation

--- src/main/java/com/smi/drools/model/CustomerDocument.java
+++ src/main/java/com/smi/drools/model/CustomerDocument.java
@@ -1,7 +1,9 @@
 package com.smi.drools.model;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import lombok.Data;
 
@@ -12,8 +14,31 @@ public class CustomerDocument implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 7692579087270502453L;
-	
+
 	private String senderEid;
 	private String receiverEid;
 	private Map<Integer, CustomerDocumentContext> customerDocumentsContextMap;
+
+	public boolean containsFractions(CustomerDocument customerDocument) {
+		boolean status = false;
+
+		Map<Integer, CustomerDocumentContext> cMap = customerDocument.getCustomerDocumentsContextMap();
+		Set<Integer> keySet = cMap.keySet();
+
+		for (Integer key : keySet) {
+			CustomerDocumentContext customerDocumentContext = cMap.get(key);
+			List<LineItemData> lineItemDatas = customerDocumentContext.getLineItemDatas();
+			for (LineItemData lineItemData : lineItemDatas) {
+				FieldValue amount = lineItemData.getAmount();
+				String value = amount.getValue();
+				value = value.replace(",", "");
+				double round = Double.parseDouble(value);
+				if (round % 1 != 0) {
+					status =  true;
+					break;
+				}
+			}
+		}
+		return status;
+	}
 }
